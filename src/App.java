8745730c8718872a20bca4dec8871caa9cfbf812
m. paz jimenez <meripeich@gmail.com>
2025-11-15 11:11:20 +0100

    cambia el nombre de la variable linea por caracter

--- src/App.java
+++ src/App.java
@@ -1,14 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-		int linea = 0;
+		int caracter = 0;
 
 		System.out.print("\033[32m");
 		for (int i = 0; i < 8000; i++) {
 			System.out.print((char)((int)(Math.random() * (126 - 32 + 1) + 32)));
 
-			if (linea++ == 100) {
-				linea = 0;
+			if (caracter++ == 100) {
+				caracter = 0;
 				Thread.sleep(50);
 				System.out.println();
 			}
